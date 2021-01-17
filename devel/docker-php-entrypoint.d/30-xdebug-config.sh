eba6e6d17b3cbf90daf9f6b31135910d654a5f50
Vlado Senasi <vladimir.senasi@nalgoo.com>
2021-01-17 18:11:37 +0100

    fixes for php8

--- devel/docker-php-entrypoint.d/30-xdebug-config.sh
+++ devel/docker-php-entrypoint.d/30-xdebug-config.sh
@@ -4,19 +4,17 @@ set -e
 
 # running php-fpm
 if [ "$1" = "php-fpm" ]; then
-	if [ -e /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
-    { \
-      echo "[Xdebug]"; \
-      echo "xdebug.mode=develop,debug,profile"; \
-      echo "xdebug.start_with_request=trigger"; \
-      if [ -z "${XDEBUG_CLIENT_HOST}" ]; then \
-        echo "xdebug.discover_client_host=true"; \
-      else \
-        echo "xdebug.client_host=${XDEBUG_CLIENT_HOST}"; \
-      fi; \
-      echo "xdebug.client_port = ${XDEBUG_CLIENT_PORT:-9003}"; \
-      echo "xdebug.output_dir=/data/log"; \
-      echo "xdebug.idekey = \"${XDEBUG_IDE_KEY:-PHPSTORM}\""; \
-    } | tee /usr/local/etc/php/conf.d/xdebug.ini
-	fi
+  { \
+    echo "[Xdebug]"; \
+    echo "xdebug.mode=develop,debug,profile"; \
+    echo "xdebug.start_with_request=trigger"; \
+    if [ -z "${XDEBUG_CLIENT_HOST}" ]; then \
+      echo "xdebug.discover_client_host=true"; \
+    else \
+      echo "xdebug.client_host=${XDEBUG_CLIENT_HOST}"; \
+    fi; \
+    echo "xdebug.client_port=${XDEBUG_CLIENT_PORT:-9003}"; \
+    echo "xdebug.output_dir=/data/log"; \
+    echo "xdebug.idekey=\"${XDEBUG_IDE_KEY:-PHPSTORM}\""; \
+  } | tee /usr/local/etc/php/conf.d/xdebug.ini
 fi
