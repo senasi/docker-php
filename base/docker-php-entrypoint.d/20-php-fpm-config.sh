716198e40ebb181327ac58b22eecfa442a3ee85c
Vlado Senasi <vladimir.senasi@nalgoo.com>
2022-10-04 09:45:52 +0200

    added new php-fpm variables

--- base/docker-php-entrypoint.d/20-php-fpm-config.sh
+++ base/docker-php-entrypoint.d/20-php-fpm-config.sh
@@ -12,6 +12,8 @@ if [ "$1" = "php-fpm" ]; then
 		echo "pm.min_spare_servers = ${FPM_MIN_SPARE_SERVERS:-2}"; \
 		echo "pm.max_spare_servers = ${FPM_MAX_SPARE_SERVERS:-10}"; \
 		echo "pm.max_requests = ${FPM_MAX_REQUESTS:-100}"; \
+		echo "pm.process_idle_timeout = ${FPM_PROCESS_IDLE_TIMEOUT:-10}"; \
+		echo "pm.max_spawn_rate = ${FPM_MAX_SPAWN_RATE:-32}"; \
 	} | tee /usr/local/etc/php-fpm.d/zzz-docker.conf
 
 	{ \
