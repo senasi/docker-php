c895ea6db107d3f52a09ff6ea8eda2b0b00417b1
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-01-31 13:58:20 +0100

    changed entrypoint to always generate php.ini, bumped versions of php to v8.2.15 and nginx to v1.25.3

--- base/docker-php-entrypoint.d/20-php-fpm-config.sh
+++ base/docker-php-entrypoint.d/20-php-fpm-config.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# running php-fpm
+# when running php-fpm
 if [ "$1" = "php-fpm" ]; then
 	{ \
 		echo "[www]"; \
@@ -15,12 +15,13 @@ if [ "$1" = "php-fpm" ]; then
 		echo "pm.process_idle_timeout = ${FPM_PROCESS_IDLE_TIMEOUT:-10}"; \
 		echo "pm.max_spawn_rate = ${FPM_MAX_SPAWN_RATE:-32}"; \
 	} | tee /usr/local/etc/php-fpm.d/zzz-docker.conf
-
-	{ \
-		echo "[php]"; \
-		echo "expose_php = off"; \
-		echo "memory_limit = ${PHP_MEMORY_LIMIT:-512M}"; \
-		echo "post_max_size = ${PHP_POST_MAX_SIZE:-256M}"; \
-		echo "upload_max_filesize = ${PHP_UPLOAD_MAX_SIZE:-128M}"; \
-	} | tee /usr/local/etc/php/php.ini
 fi
+
+# always
+{ \
+  echo "[php]"; \
+  echo "expose_php = off"; \
+  echo "memory_limit = ${PHP_MEMORY_LIMIT:-512M}"; \
+  echo "post_max_size = ${PHP_POST_MAX_SIZE:-256M}"; \
+  echo "upload_max_filesize = ${PHP_UPLOAD_MAX_SIZE:-128M}"; \
+} | tee /usr/local/etc/php/php.ini
